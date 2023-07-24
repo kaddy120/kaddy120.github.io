92f1512b817a32c2ccd8dc865c71fc2041458283
kaddy120 <kaddy120@gmail.com>
2023-07-24 23:33:18 +0200

    humber-menu, first version

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function copyEmail() {
   document.body.removeChild(tempInput);
   alert("Email copied to clipboard: " + email);
 }
+
+
+function toggleMenu() {
+const menu = document.getElementById("nav-list");
+  console.log(menu, "menu");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+  /* menu.classList.append("showMenu"); */
+}
+
