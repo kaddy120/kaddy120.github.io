5a770a6956a451d336c6fcf9827ec2ff00cf10b6
kaddy120 <kaddy120@gmail.com>
2023-08-02 16:48:49 +0200

    when you click tocLink, scroll to the right position

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function copyEmail() {
-  
-}
+function copyEmail() {}
 
 function toggleMenu() {
   const menu = document.getElementById('nav-list');
@@ -20,53 +18,89 @@ function toggleMenu() {
   /* menu.classList.append("showMenu"); */
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const navbarLinks = document.querySelectorAll('nav a');
   const sections = document.querySelectorAll('section');
 
+  const tocLinks = document.querySelectorAll('.toc_anchor');
+  const headingSections = document.querySelectorAll('.markdown-body h2, h3');
+
+  /* const sections = document.querySelectorAll('section'); */
   // Function to check if an element is in the viewport
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     const windowHeight =
       window.innerHeight || document.documentElement.clientHeight;
-    const windowWidth =
-      window.innerWidth || document.documentElement.clientWidth;
     const vertInView =
       rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2;
-    const horInView =
-      rect.left <= windowWidth / 2 && rect.right >= windowWidth / 2;
-    return vertInView && horInView;
+    return vertInView;
   }
 
   // Function to update the active link based on the current section in the viewport
-  function updateActiveLink() {
+  function updateActiveLink(sections, highlightActive) {
     sections.forEach((section) => {
       const sectionId = section.getAttribute('id');
+      /* const correspondingLink = document.querySelector( */
+      /*   `nav a[href="#${sectionId}"]` */
+      /* ); */
       const correspondingLink = document.querySelector(
-        `nav a[href="#${sectionId}"]`
+        `a[href="#${sectionId}"]`
       );
 
       if (isInViewport(section)) {
         // Remove the "active" class from all links
         navbarLinks.forEach((link) => {
-          link.classList.remove('active');
+          link.classList.remove(highlightActive);
         });
 
         // Add the "active" class to the corresponding link
-        correspondingLink.classList.add('active');
+        correspondingLink.classList.add(highlightActive);
+      }
+    });
+  }
+
+  function updateTocActiveLink(sections, highlightActive) {
+    sections.forEach((section) => {
+      const sectionId = section.getAttribute('id');
+      /* const correspondingLink = document.querySelector( */
+      /*   `nav a[href="#${sectionId}"]` */
+      /* ); */
+      const correspondingLink = document.querySelector(
+        `a[href="#${sectionId}"]`
+      );
+
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const vertInView = windowHeight / 12 <= rect.top && (windowHeight/12)+ 250 >= rect.bottom ;
+        /* && rect.bottom >= windowHeight / 4; */
+
+      if (vertInView) {
+        // Remove the "active" class from all links
+        tocLinks.forEach((link) => {
+          link.classList.remove(highlightActive);
+        });
+          correspondingLink.classList.add(highlightActive);
+        // Add the "active" class to the corresponding link
       }
     });
   }
 
   // Update active link on initial page load
-  updateActiveLink();
+  updateActiveLink(sections, 'active');
+  updateTocActiveLink(headingSections, 'toc_highlight');
 
   // Update active link on scroll
-  window.addEventListener('scroll', updateActiveLink);
+  window.addEventListener('scroll', function () {
+    updateActiveLink(sections, 'active');
+  });
+
+  window.addEventListener('scroll', function () {
+    updateTocActiveLink(headingSections, 'toc_highlight');
+  });
 
   // Handle link click events
   navbarLinks.forEach((link) => {
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', function (event) {
       const href = this.getAttribute('href');
       if (href[0] === '#') event.preventDefault(); // Prevent default link navigation
 
@@ -77,6 +111,27 @@ document.addEventListener('DOMContentLoaded', function() {
         link.classList.remove('active');
       });
       this.classList.add('active');
+      // Add the "active" class to the clicked link
+
+      // Scroll to the target section after a small delay (e.g., 300ms)
+      document.querySelector(targetSectionId).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+
+  tocLinks.forEach((link) => {
+    link.addEventListener('click', function (event) {
+      const href = this.getAttribute('href');
+      if (href[0] === '#') event.preventDefault(); // Prevent default link navigation
+
+      const targetSectionId = this.getAttribute('href');
+
+      // Remove the "active" class from all links
+      tocLinks.forEach((link) => {
+        link.classList.remove('toc_highlight');
+      });
+      this.classList.add('toc_highlight');
       // Add the "active" class to the clicked link
 
       // Scroll to the target section after a small delay (e.g., 300ms)
