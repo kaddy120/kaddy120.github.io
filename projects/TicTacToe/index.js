11b214741534a014e453730e6925cd17732bab91
kaddy120 <kaddy120@gmail.com>
2023-08-08 23:51:49 +0200

    Alt turns

--- projects/TicTacToe/index.js
+++ projects/TicTacToe/index.js
@@ -18,17 +18,33 @@ function Gameboard() {
 const gameboard = Gameboard();
 
 const GameLogic = ((gameboard) => {
+  let _results = document.querySelector('.results');
+  let _winner = document.querySelector('#winner');
+  let _restartButton = document.querySelector('#restart');
+  let _turn = document.querySelector('#turn');
   let _isPlayer1Turn = true;
+  let _movesCount = 0;
   const _boxes = document.querySelectorAll('.box');
 
   const run = () => {
+    _restartButton.addEventListener('click', function () {
+      gameboard.fill(0);
+      _movesCount = 0;
+      _boxes.forEach((box) => {
+        box.innerHTML = '<h2></h2>';
+      });
+      _results.classList.add('hide');
+    });
+
     _boxes.forEach((box) => {
-      box.addEventListener('click', function () {
+      box.addEventListener('click', function j() {
         let move;
         if (_isPlayer1Turn) {
           move = 'X';
+          _turn.innerHTML ='X'
         } else {
           move = 'O';
+          _turn.innerHTML ='O'
         }
 
         const id = this.getAttribute('id');
@@ -38,16 +54,23 @@ const GameLogic = ((gameboard) => {
         if (isSquareTaken(index)) return;
 
         gameboard[index] = move;
-        this.innerHTML = `<h3>${move}</h3>`;
+        this.innerHTML = `<h2 class="${move}">${move}</h2>`;
 
+        _movesCount++;
+        /* Okay, the only thing that I'm left with is enuring that a player doesn't continue playing after a win */
         if (checkWin(move)) {
-          window.alert("You have won")
-          /* give a winner message, */
-          /* and then allow the player to reset the game.  */
+          _results.classList.remove('hide');
+          _winner.innerHTML = move;
+          return;
+        } else if (_movesCount === 9) {
+          _results.classList.remove('hide');
+          let winner = document.getElementsByClassName('winner');
+          winner.innerHTML = "";
+          return;
         }
 
         _isPlayer1Turn = !_isPlayer1Turn;
-        console.log(_isPlayer1Turn)
+        console.log(_isPlayer1Turn);
       });
     });
   };
@@ -85,8 +108,10 @@ const GameLogic = ((gameboard) => {
     }
 
     /* check if there's a diagonal win */
-    if (gameboard[0] == move && gameboard[4] == move && gameboard[8] == move) return true;
-    if (gameboard[6] == move && gameboard[4] == move && gameboard[2] == move) return true;
+    if (gameboard[0] == move && gameboard[4] == move && gameboard[8] == move)
+      return true;
+    if (gameboard[6] == move && gameboard[4] == move && gameboard[2] == move)
+      return true;
   };
 
   const isSquareTaken = (index) => {
