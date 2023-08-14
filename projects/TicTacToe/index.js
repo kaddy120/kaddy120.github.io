084f2897d5ad35381b5534e551fdc040a9cfaba8
kaddy120 <kaddy120@gmail.com>
2023-08-14 12:18:26 +0200

    show turn

--- projects/TicTacToe/index.js
+++ projects/TicTacToe/index.js
@@ -17,34 +17,39 @@ function Gameboard() {
 
 const gameboard = Gameboard();
 
+/* What  i want to do here is to crate to player, human, or bot.  */
+/* keep a record of each player score */
+/* give each player a name */
+
+function Player(){
+
+}
+
 const GameLogic = ((gameboard) => {
   let _results = document.querySelector('.results');
   let _winner = document.querySelector('#winner');
   let _restartButton = document.querySelector('#restart');
-  let _turn = document.querySelector('#turn');
+  let _turnX = document.querySelector('.player-X');
+  let _turnO = document.querySelector('.player-O');
   let _isPlayer1Turn = true;
   let _movesCount = 0;
   const _boxes = document.querySelectorAll('.box');
 
-  const run = () => {
-    _restartButton.addEventListener('click', function () {
-      gameboard.fill(0);
-      _movesCount = 0;
-      _boxes.forEach((box) => {
-        box.innerHTML = '<h2></h2>';
-      });
-      _results.classList.add('hide');
-    });
 
+  const run = () => {
+    restart();
     _boxes.forEach((box) => {
-      box.addEventListener('click', function j() {
+      box.addEventListener('click', function() {
+        console.log('you clicked me');
         let move;
         if (_isPlayer1Turn) {
           move = 'X';
-          _turn.innerHTML ='X'
+          turn(move);
+          /* _turn.innerHTML ='X' */
         } else {
           move = 'O';
-          _turn.innerHTML ='O'
+          turn(move);
+          /* _turn.innerHTML ='O' */
         }
 
         const id = this.getAttribute('id');
@@ -65,13 +70,36 @@ const GameLogic = ((gameboard) => {
         } else if (_movesCount === 9) {
           _results.classList.remove('hide');
           let winner = document.getElementsByClassName('winner');
-          winner.innerHTML = "";
+          winner.innerHTML = '';
           return;
         }
 
         _isPlayer1Turn = !_isPlayer1Turn;
         console.log(_isPlayer1Turn);
       });
+    });
+  };
+
+  const turn = (move) => {
+    if (move == 'X') {
+      _turnO.classList.add('playing');
+      _turnX.classList.remove('playing');
+    } else {
+      _turnO.classList.remove('playing');
+      _turnX.classList.add('playing');
+    }
+  };
+
+  const restart = () => {
+    _restartButton.addEventListener('click', function() {
+      /* show that it's X turn  */
+      turn('O');
+      gameboard.fill(0);
+      _movesCount = 0;
+      _boxes.forEach((box) => {
+        box.innerHTML = '<h2></h2>';
+      });
+      _results.classList.add('hide');
     });
   };
 
